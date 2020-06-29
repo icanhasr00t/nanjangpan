6fe447d253266cdfad7ac8957e3abe77d5cbb9ce
icanhasr00t <icanhasr00t@gmail.com>
2020-06-30 07:49:58 +0900

    Dark mode enabled

---
+++ ui/light/read.js
@@ -0,0 +1,124 @@
+const vm = new Vue({
+    el: '#app',
+    data() {
+        return {
+            nakseo: {},
+            replies: [],
+            start_at:  -1,
+            // TODO: do something about stopping the IS
+        };
+    },
+    mounted() {
+        this.getItem();
+        // infinite scroll
+        window.onscroll = () => {
+            let bottomOfWindow =
+                document.documentElement.scrollTop + window.innerHeight ===
+                document.documentElement.offsetHeight;
+
+            if (bottomOfWindow) {
+                if (this.replies.length > 0) {
+                    this.start_at = this.replies[this.replies.length - 1].id;
+					console.log(this.start_at);
+                    this.getMoreReplies();
+                }
+            }
+        };
+
+    },
+    methods: {
+        highlight: function (content) {
+            const DELIM = "```";
+            if (content && content.length > 0) {
+                let pos = content.indexOf(DELIM);
+                if (pos >= 0) {
+                    const tokens = content.split(DELIM);
+                    let result = tokens[0];
+                    for (let i = 1; i < tokens.length; i++) {
+                        if ((i & 1) != 0) {
+                            result += "<div class='hljs'><pre><code>\n";
+                        } else {
+                            result += "\n</code></pre></div>\n";
+                        }
+                        result += tokens[i];
+                    }
+                    let dom = htmlToElem(result);
+                    $(dom).ready(function () {
+                        $('pre code').each(function (i, e) {
+                            hljs.highlightBlock(e);
+                        });
+                    });
+                    return result;
+                }
+            }
+            return content;
+        },
+        getItem: function () {
+            let id = new URLSearchParams(window.location.search).get('id');
+            check4Id(id);
+            let uri = `${ENDPOINT}${id}`;
+            axios.get(uri)
+                .then((res) => {
+                    this.nakseo = res.data;
+                    this.replies = res.data.replies;
+                    this.nakseo.at = new Date(this.nakseo.at).toLocaleString()
+                })
+                .catch((err) => {
+                    triggerError();
+                    window.location.href = "/index.html";
+                });
+        },
+        tryRm: function () {
+            let uri = `${ENDPOINT}${this.nakseo.id}`;
+            axios.delete(uri, { data: { passwd: this.nakseo.passwd } })
+                .then((res) => {
+                    $('#askPwd').modal('hide');
+                    window.location.href = "/index.html";
+                })
+                .catch((err) => {
+                    triggerError();
+                });
+        },
+        editItem: function (id) {
+            window.location.href = `/edit.html?id=${id}`;
+        },
+        reply: function (event) {
+			if (event) {
+				event.preventDefault();
+        		event.stopPropagation();
+			}
+			const rpl = this.nakseo.reply;
+            this.nakseo.reply = "";
+
+			if (!rpl || rpl === "" || rpl.length === 0) {
+				return;
+			}
+
+            const uri = `${ENDPOINT}${this.nakseo.id}`;
+            const data = {
+                body: rpl,
+            }
+            axios.post(uri, data)
+                .then((res) => {
+                    this.replies.splice(0, 0, { id: res.data.id, body: rpl });
+                    this.nakseo.reply = "";
+                })
+                .catch((err) => {
+                    triggerError("#errReply");
+                });
+        },
+        getMoreReplies: function () {
+            const uri = `${ENDPOINT}${this.nakseo.id}`;
+            headers = {
+                'Start-At': (this.start_at == -1) ? '' : this.start_at,
+            };
+            axios.get(uri, { headers })
+                .then((res) => {
+                    this.replies.push(...res.data)
+                })
+                .catch((e) => {
+                    triggerError("#errReply");
+                });
+        },
+    }
+});
---
+++ ui/light/shared.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+    $("#bCancel").click(function (e) {
+        e.preventDefault();
+        window.history.back();
+    });
+});
+
+const ENDPOINT = "https://wtf.codentalks.com/nanjangpan/"
+
+const getDocHeight = () => {
+    const D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    );
+};
+
+const nl2br = (str, is_xhtml) => {
+    var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+};
+
+const htmlToElem = (html) => {
+    const template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+};
+
+const check4Id = (id) => {
+    if (!id || isNaN(id)) {
+        window.history.back();
+        return;
+    }
+};
+
+const triggerError = (target = "#errNote") => {
+    $(target).fadeTo(1000, 500).slideUp(100, function () {
+        $(target).slideUp(100);
+    });
+};
+
+const setupEditor = () => {
+    $("#summernote").summernote({
+        placeholder: "내용",
+        codeviewFilter: false,
+        codeviewIframeFilter: true,
+        tabsize: 2,
+        height: 400,
+        callbacks: {
+            onImageUpload: function (files) {
+                for (let i = 0; i < files.length; i++) {
+                    upload(files[i]);
+                }
+            },
+            onEnter: function(e) {
+                $(this).summernote("pasteHTML", "<br><br>");
+                e.preventDefault();
+            },
+        },
+    });
+};
+
+const upload = (file) => {
+    let out = new FormData();
+    out.append("file", file, file.name);
+    $.ajax({
+        method: "POST",
+        url: `${ENDPOINT}upload`,
+        contentType: false,
+        cache: false,
+        processData: false,
+        data: out,
+        success: function (img) {
+            $("#summernote").summernote("insertImage", img);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error(textStatus + " " + errorThrown);
+            triggerError();
+        },
+    });
+};
