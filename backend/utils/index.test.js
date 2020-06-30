d27191057588260601ed3a94b1a11aaff312747d
icanhasr00t <icanhasr00t@gmail.com>
2020-07-01 01:14:30 +0900

    XSS filter tuned

--- backend/utils/index.test.js
+++ backend/utils/index.test.js
@@ -55,7 +55,7 @@ test("sanitizer test - empty body 3", () => {
 test("sanitizer test - js events", () => {
 	const testData = `<img src="zzz" onerror = "alert('hi')"><img src="zzz" onLoad = "alert('hi')">`;
 	const actual = utils.sanitize(testData);
-	const expected = `<img src="zzz"  = "alert('hi')"><img src="zzz"  = "alert('hi')">`;
+	const expected = `<img src="zzz" "alert('hi')"><img src="zzz" "alert('hi')">`;
 	expect(actual).toBe(expected);
 });
 
@@ -84,4 +84,23 @@ test("check4EmptyStrings test - ok", () => {
 		fail('it should not reach here');
 	} catch (e) {
 	}
+});
+
+test("On blar removal test", () => {
+   const testData = [
+	`<img src="about:blank" style="display: none;" ononerrorerror="alert('XSS&#32;테스트')">`,
+	`<img src="about:blank" style="display: none;" onclick='alert('XSS&#32;테스트')'>`,
+	`<img src="about:blank" style="display: none;" ononerrorfocus="alert('XSS&#32;테스트')">`,
+	`<img src="about:blank" style="display: none;" ononerrorerror = "alert('XSS&#32;테스트')">`,
+	`<img src="about:blank" style="display: none;" onclick ='alert('XSS&#32;테스트')'>`,
+	`<img src="about:blank" style="display: none;" ononerrorfocus= "alert('XSS&#32;테스트')">`,
+    `<img src="about:blank" style="display:none;" data-my="test"/onerror="alert()">`,
+	];
+
+	testData.forEach((v) => {
+		const data = utils.removeEvents(v);
+		if (utils.REX_EVENT_TARGET.test(data)) {
+			fail(`case failed with ${v}`);
+		}
+	});
 });
